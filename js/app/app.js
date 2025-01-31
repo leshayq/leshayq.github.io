43568c0de2d6ed02dd8c42cf67ec903470cf9275
eclipse_mint <oleshadesigner123@gmail.com>
2025-01-31 12:50:55 +0200

    day 2

--- js/app/app.js
+++ js/app/app.js
@@ -1,4 +1,5 @@
 import { router } from './router.js';
+import {msg} from './widgets/msg.js';
 
 document.addEventListener('DOMContentLoaded', function () {
     const main = {
@@ -21,12 +22,84 @@ document.addEventListener('DOMContentLoaded', function () {
             this.init();
         },
         methods: {
-            init() {
+            init(){
+                var self = this;
+                if(window.localStorage.getItem('user')) self.user = JSON.parse(window.localStorage.getItem('user'));
+            
+                router.isReady().then(() => {
+                    if(window.localStorage.getItem("user")){
+                        self.user = JSON.parse(window.localStorage.getItem("user"));
+                        if(self.$route['path']=='/' && self.user.type=='admin'){
+                            self.page('/campaigns');
+                        }else if(['/campaigns','/campaign','/users','/user'].includes(self.$route['path']) && self.user.type!='admin'){
+                            self.page('/statistics');
+                        }else if(['/statistics','/payments','/sites'].includes(self.$route['path']) && self.user.type=='admin'){
+                            self.page('/campaigns');
+                        }else if(['/campaigns','/campaign','/users','/user','/statistics','/payments','/sites'].includes(self.$route['path'])){
+                            self.page();
+                        }else if(!['/campaigns','/campaign','/users','/user','/statistics','/payments','/sites'].includes(self.$route['path'])){
+                            self.page();
+                        }
+                    }else{
+                        self.page('/');
+                    }
+                });
             },
+            logout() {
+                this.user = {name: "", phone: "", email: "", date: "", auth: ""};
+                this.page('/');
+                window.localStorage.setItem('user', '');
+            },
+
+            scrollTop() {
+                setTimeout(function() {
+                    window.scroll({
+                        top: 0,
+                        behavior: 'smooth'
+                    });
+                }, 50);
+            },
+
+            scrollBottom() {
+                setTimeout(function() {
+                    window.scroll({
+                        top: 1000,
+                        behavior: 'smooth'
+                    });
+                }, 50);
+            },
+
+            page: function (path="") {
+                this.$router.replace(path);
+                this.title = this.$route['name'];
+                document.title = this.$route['name'];
+            },
+            toFormData: function(obj){
+                var fd = new FormData();
+
+                for (var x in obj) {
+                    if (typeof obj[x] === 'object' && x !== 'img' && x !== 'copy') {
+                        for (var y in obj[x]) {
+                            if (typeof obj[x][y] === 'object') {
+                                for (var z in obj[x][y]) {
+                                    fd.append(x + '[' + y + '][' + z + ']', obj[x][y][z]);
+                                }
+                            } else {
+                                fd.append(x + '[' + y + ']', obj[x][y]);
+                            }
+                        }
+                    } else if (x !== 'copy') {
+                        fd.append(x, obj[x]);
+                    }
+                }
+
+                return fd;
+            }
         },
     };
 
     var app = Vue.createApp(main)
+        .component('msg',msg)
         .use(router)
         .mount('#content');
 });
--- js/app/pages/login.js
+++ js/app/pages/login.js
@@ -2,7 +2,6 @@ export const login = {
         data: function() {
             return {
                 img:1,
-                hs:0,
                 parent:''
             }
         },
@@ -11,29 +10,64 @@ export const login = {
             this.parent = this.$parent.$parent
         },
         methods:{
-            // randomIntFromInterval:function (min,max) {
-            //     return Math.floor(Math.random() * (max - min + 1) + min);
-            // },
-            // login:function(){
-            //     var self = this;
-            //     var data = self.parent.toFormData(self.parent.formData);
+            randomIntFromInterval:function (min,max) {
+                return Math.floor(Math.random() * (max - min + 1) + min);
+            },
+            login:function(){
+                var self = this;
+                var data = self.parent.toFormData(self.parent.formData);
 
-            //     axios.post(this.parent.url+"/site/login",data).then(function(response){
-            //         if(response.data.error){
-            //             self.$refs.msg.alertFun(response.data.error)
-            //         }
-            //         if(response.data.user){
-            //             self.parent.user = response.data.user;
-            //             self.parent.page('/campaigns');
-            //             window.localStorage.setItem('user', JSON.stringify(self.parent.user));
-            //         }
-            //     }).catch(function(error){
-            //         console.log('errors: ', error);
-            //     });
-            // },
+                axios.post(this.parent.url+"/site/login",data).then(function(response){
+                    if(response.data.error){
+                        self.$refs.msg.alertFun(response.data.error)
+                    }
+                    if(response.data.user){
+                        self.parent.user = response.data.user;
+                        self.parent.page('/campaigns');
+                        window.localStorage.setItem('user', JSON.stringify(self.parent.user));
+                    }
+                }).catch(function(error){
+                    console.log('errors: ', error);
+                });
+            },
         },
         template: `
         <div class="flex">
-            kadsfkaskfksdfksfk
+    <msg ref="msg"/>
+    
+    <div id="left-area">
+        <img :src="parent.url+'/app/views/images/Cover_'+img+'.jpg'" />
+    </div>
+
+    <div id="right-container">
+        <div class="header">
+            <div class="wrapper">
+                <h1>Affiliate Sign in</h1>
+                <div class="logo">
+                    <img :src="parent.url+'/app/views/images/logo.svg'" />
+                </div>
+            </div>
         </div>
+
+        <div id="right-area">
+            <div class="form">
+                <form @submit.prevent="login()" v-if="parent.formData">
+                    <div class="row">
+                        <label>Email</label>
+                        <input type="email" v-model="parent.formData.email" required>
+                    </div>
+
+                    <div class="row">
+                        <label>Password</label>
+                        <input type="password" v-model="parent.formData.password" required autocomplete="on">
+                    </div>
+
+                    <div class="row">
+                        <button class="btn">SIGN IN</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
 `};
